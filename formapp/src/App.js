367e71b36bd48bb333081b5fed249d299dbda613
Gabriel Coelho <gabriellvs60@gmail.com>
2019-10-15 15:47:46 -0300

    Initial commit

---
+++ formapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactData from './containers/ContactData/ContactData'
+
+function App() {
+  return (
+    <div className="App">
+      <ContactData/>
+    </div>
+  );
+}
+
+export default App;
